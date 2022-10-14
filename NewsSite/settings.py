c38def9f9c67d3b26d9657362dbe9095aa128119
Bullochka <tamer.kab@mail.ru>
2022-10-14 19:07:31 +0200

    Deploy4

--- NewsSite/settings.py
+++ NewsSite/settings.py
@@ -19,7 +19,7 @@ ALLOWED_HOSTS = ['*']
 
 
 db_from_env = dj_database_url.config()
-DATABASE['default'].update(db_from_env)
+DATABASES['default'].update(db_from_env)
 
 # Application definition
 
@@ -193,3 +193,6 @@ CKEDITOR_CONFIGS = {
         ]),
     }
 }
+
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
