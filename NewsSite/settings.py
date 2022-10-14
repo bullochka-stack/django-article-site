0fd9d2e64966244a4ece1c19da02130d18461e85
Bullochka <tamer.kab@mail.ru>
2022-10-14 19:57:55 +0200

    Deploy6

--- NewsSite/settings.py
+++ NewsSite/settings.py
@@ -119,10 +119,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'NewsSite/static'),
-]
+
+if DEBUG:
+   STATICFILES_DIRS = [
+   os.path.join(BASE_DIR, 'static'),
+   ]
+else:
+   STATIC_ROOT = os.path.join(BASE_DIR,'NewsSite/static')
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
