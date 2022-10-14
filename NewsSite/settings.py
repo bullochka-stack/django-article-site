1b0130e3b2be9178fbae06c2621b7a59df42ce40
Bullochka <tamer.kab@mail.ru>
2022-10-14 18:53:54 +0200

    Deploy2

--- NewsSite/settings.py
+++ NewsSite/settings.py
@@ -3,7 +3,8 @@ Django settings for NewsSite project.
 """
 import os
 from pathlib import Path
-import dj-database-url
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
